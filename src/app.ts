087981f8bc15cadc19c7e65092cf7a964cb84178
Fidela 1 <fidelatuyizere4@gmail.com>
2026-05-24 19:15:08 +0200

    Deployment

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import passport from './config/passport'
-import mainRoute from './routes/server.route'
+import passport from './config/passport';
+import mainRoute from './routes/server.route';
 import session from 'express-session';
 import path from 'path';
+import fs from 'fs';
 
 dotenv.config();
 
 const app = express();
 
+const uploadsDir = path.join('/tmp', 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
 app.use(cors({
-  origin: "*",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
@@ -20,14 +26,13 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, 
-    maxAge: 24 * 60 * 60 * 1000 
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 24 * 60 * 60 * 1000
   }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,17 +40,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use('/api/v1', mainRoute);
 
-const startServer = async () => {
-  try {
+export default app;
 
-    const PORT = process.env.PORT || 3030;
-    app.listen(PORT, () => {
-      console.log(` Server running on http://localhost:${PORT}`);
-    });
-
-  } catch (error) {
-    console.error(" Failed to start server:", error);
-  }
-};
-
-startServer();
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3030;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
